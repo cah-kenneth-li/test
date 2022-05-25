33984ffcdbe40ee5cccfc9625f76d28ff429e08e
Kenneth Li <kenneth.li@cardinalhealth.com>
2022-05-25 14:28:27 -0400

    having trouble with express

--- test_app/src/App.js
+++ test_app/src/App.js
@@ -27,3 +27,60 @@ function App() {
 }
 
 export default App;
+
+// import React, {useState, useEffect} from 'react';
+// function App() {
+//   const [merchants, setMerchants] = useState(false);
+//   useEffect(() => {
+//     getMerchant();
+//   }, []);
+//   function getMerchant() {
+//     fetch('http://localhost:3001')
+//       .then(response => {
+//         return response.text();
+//       })
+//       .then(data => {
+//         setMerchants(data);
+//       });
+//   }
+//   function createMerchant() {
+//     let name = prompt('Enter name');
+//     fetch('http://localhost:3001/merchants', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({name}),
+//     })
+//       .then(response => {
+//         return response.text();
+//       })
+//       .then(data => {
+//         alert(data);
+//         getMerchant();
+//       });
+//   }
+//   function deleteMerchant() {
+//     let name = prompt('Enter merchant name');
+//     fetch(`http://localhost:3001/merchants/${name}`, {
+//       method: 'DELETE',
+//     })
+//       .then(response => {
+//         return response.text();
+//       })
+//       .then(data => {
+//         alert(data);
+//         getMerchant();
+//       });
+//   }
+//   return (
+//     <div>
+//       {merchants ? merchants : 'There is no merchant data available'}
+//       <br />
+//       <button onClick={createMerchant}>Add merchant</button>
+//       <br />
+//       <button onClick={deleteMerchant}>Delete merchant</button>
+//     </div>
+//   );
+// }
+// export default App;
