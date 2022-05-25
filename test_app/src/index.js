7405005e4dc2f3be1c51829de2ff44435909f3b7
Kenneth Li <kenneth.li@cardinalhealth.com>
2022-05-25 10:34:20 -0400

    test

--- test_app/src/index.js
+++ test_app/src/index.js
@@ -5,11 +5,25 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
